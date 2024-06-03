be2b8e9705b8b91e34bab23efd5ee5e8dd311cef
Mangesh98 <mangeshraje55555@gmail.com>
2024-06-03 10:34:25 +0530

    Form Created for tasks

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-	res.send("Hello World");
+	res.render("index");
+});
+app.get("/profile/:username", function (req, res) {
+	res.send("hii "+req.params.username);
 });
 
 app.listen(3000);
