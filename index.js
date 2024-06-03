20b818e58c3844e334cc04e215119462389822ae
Mangesh98 <mangeshraje55555@gmail.com>
2024-06-03 11:05:27 +0530

    Task create ✔

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,10 +9,18 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-	res.render("index");
+	fs.readdir(`./files`, function (err, files) {
+		res.render("index", { files: files });
+	});
 });
-app.get("/profile/:username", function (req, res) {
-	res.send("hii "+req.params.username);
+app.post("/create", function (req, res) {
+	fs.writeFile(
+		`./files/${req.body.title.split(" ").join("")}.txt`,
+		req.body.details,
+		function (err) {
+			res.redirect("/");
+		}
+	);
 });
 
 app.listen(3000);
